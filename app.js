1380ba6e08bd7f062dbcba649b2967d954e3d05a
Yokky9 Macbook Pro <admin@Yokky9-MacBook-Pro.local>
2024-03-23 00:17:09 +0700

    update env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const router = require('./routes/myRouter')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -11,5 +11,5 @@ app.use(router)
 app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(port, ()=>{
-    console.log("Strat server at port 8000");
+    console.log(`Strat server at port ${port}`);
 })
